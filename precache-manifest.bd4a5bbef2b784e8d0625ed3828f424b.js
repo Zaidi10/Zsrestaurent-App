89ff17e59df22b7d4010d5bc55fc4bc2eadcccaa
Zaidi10 <16ume051@lnmiit.ac.in>
2020-10-01 12:40:28 +0530

    Updates

---
+++ precache-manifest.bd4a5bbef2b784e8d0625ed3828f424b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72566137b2643fa499028eb27fd9fdf4",
+    "url": "/Zsrestaurent-App/index.html"
+  },
+  {
+    "revision": "a87758f6f1eb266c54bc",
+    "url": "/Zsrestaurent-App/static/css/2.33ae93a6.chunk.css"
+  },
+  {
+    "revision": "462ac0b336a6f19ac4f1",
+    "url": "/Zsrestaurent-App/static/css/main.26eca4d8.chunk.css"
+  },
+  {
+    "revision": "a87758f6f1eb266c54bc",
+    "url": "/Zsrestaurent-App/static/js/2.dbda9249.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Zsrestaurent-App/static/js/2.dbda9249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462ac0b336a6f19ac4f1",
+    "url": "/Zsrestaurent-App/static/js/main.3a90f0c0.chunk.js"
+  },
+  {
+    "revision": "93ba0100b867480d74f9",
+    "url": "/Zsrestaurent-App/static/js/runtime-main.0abe9f8e.js"
+  },
+  {
+    "revision": "c4987958d014a701af6c18ab5aecfa10",
+    "url": "/Zsrestaurent-App/static/media/avatar1.c4987958.jpg"
+  },
+  {
+    "revision": "83c658830272bbcedb93597f4c89c8b0",
+    "url": "/Zsrestaurent-App/static/media/cover-light .83c65883.jpg"
+  },
+  {
+    "revision": "dcda508776051f1cb5f795a749e46c19",
+    "url": "/Zsrestaurent-App/static/media/cover.dcda5087.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Zsrestaurent-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Zsrestaurent-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Zsrestaurent-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Zsrestaurent-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Zsrestaurent-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
